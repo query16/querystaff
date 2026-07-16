4852b877c8f6d58ce842d74e86d92701ba073251
query16 <querymickael@gmail.com>
2026-07-17 01:09:09 +0200

    Supprime le doublon Espace client

--- components/MainMenu.tsx
+++ components/MainMenu.tsx
@@ -71,18 +71,7 @@ export default function MainMenu() {
               {link.label}
             </a>
           ))}
-        <a
-          href="/connexion"
-          onClick={() => setOpen(false)}
-          style={{
-            color: "white",
-            textDecoration: "none",
-            fontWeight: 700,
-            whiteSpace: "nowrap",
-          }}
-        >
-          Espace client
-        </a>
+        
 
 
           <a
