2db14fc097a5d756fbdd2ec743b4248f482ad89d
query16 <querymickael@gmail.com>
2026-07-21 23:03:28 +0200

    Forcer l actualisation des collaborateurs

--- app/espace-client/collaborateurs/page.tsx
+++ app/espace-client/collaborateurs/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { createClient } from "../../../lib/supabase/server";
-
+export const dynamic = "force-dynamic";
 export default async function CollaborateursPage() {
     const supabase = await createClient();
 
